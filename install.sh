7823d555a9a66306aca11538c2b6e5c7134b96c1
Matt Swasey <mig@cake.(none)>
2008-03-14 13:50:06 -0400

    installer script is pretty crude, need to clean it up

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ sudo update-mime-database /usr/share/mime
 sudo cp erb.lang /usr/share/gtksourceview-2.0/language-specs/
 mkdir -p ~/.gnome2/gedit
 cp snippets ~/.gnome2/gedit/
+cp snippets/* ~/.gnome2/gedit/snippets/
