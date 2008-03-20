c5d76fd70e3290bad8fe086710e13784fce549ee
Matt Swasey <mig@ccrunch.(none)>
2008-03-20 12:37:27 -0400

    script is even dirtier

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo update-mime-database /usr/share/mime
 sudo cp erb.lang /usr/share/gtksourceview-2.0/language-specs/
 mkdir -p ~/.gnome2/gedit
 cp snippets ~/.gnome2/gedit/
+mkdir -p ~/.gnome2/gedit/snippets
 cp snippets/* ~/.gnome2/gedit/snippets/
